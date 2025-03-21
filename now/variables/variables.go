5ff685f257544c73b7972e96678109d084b42a70
chinthaka senarathna <chironprabath@gmail.com>
2025-03-21 21:58:29 +0530

    variables ?

--- now/variables/variables.go
+++ now/variables/variables.go
@@ -3,31 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var firstNumber int = 100
-	var secondNumber int = 11
 
-	fmt.Println(firstNumber + secondNumber)
-	fmt.Println(firstNumber - secondNumber)
-	fmt.Println(firstNumber * secondNumber)
-	fmt.Println(firstNumber / secondNumber)
-	fmt.Println(firstNumber % secondNumber)
+	var firstName, lastName = "chinthaka", "senarathna"
+	fmt.Println(firstName, lastName)
 
-	var userName string = "chinthaka senarathna"
-
-	fmt.Println("hello... " + userName)
-
-	var firstNumber_ float32 = 1.2
-	var secondNumber_ float64 = 1.23
-
-	fmt.Printf("Type : %T\n", firstNumber_)
-	fmt.Printf("Type : %T\n", secondNumber_)
-
-	var isUser bool = true
-	if isUser {
-		println("This is the User")
-	}
-
-	var firstName, secondName = "chinthaka", "senarathna"
-	fmt.Println(firstName, secondName)
+	userName := "chiron"
+	fmt.Println(userName)
 
 }
